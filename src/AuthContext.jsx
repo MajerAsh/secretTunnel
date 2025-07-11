81f7881413972a914acebb398a29a93173e3aa23
Ashley Majer <Majerash@gmail.com>
2025-07-11 17:53:22 -0400

    secret tunnel

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -1,23 +1,87 @@
 import { createContext, useContext, useState } from "react";
 
 const API = "https://fsa-jwt-practice.herokuapp.com";
+const PASSWORD = "password";
 
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-  const [token, setToken] = useState();
-  const [location, setLocation] = useState("GATE");
+  const [token, setToken] = useState(); //Stores token. norm: state, setState = useState
+  const [location, setLocation] = useState("GATE"); // Track user location in the app
 
-  // TODO: signup
+  /* 1)signup func takes in a username to send to the POST 
+  /signup endpoint and set the location to "TABLET" */
+  async function signup(username) {
+    try {
+      const response = await fetch(API + "/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username, // from inpt
+          password: PASSWORD,
+        }),
+      });
+      const result = await response.json();
+      console.log("Signup:", result);
 
-  // TODO: authenticate
+      /*Check the success property in API and
+  "if result success is true {
+  save the token in react state and
+  update location to 'tablet'/ show the next screen 
+  OR ELSE,
+  give a error message"
+  */
+      if (result.success) {
+        setToken(result.token);
+        setLocation("TABLET");
+      } else {
+        throw new Error(result.message);
+      }
+    } catch (error) {
+      console.error("Signup failed:", error);
+    }
+  }
 
-  const value = { location };
+  //********************************from index */
+  /*B) Sends a GET request to /authenticate.
+Attaches the token in the header using Bearer <token> format.
+*/
+  async function authenticate() {
+    try {
+      const response = await fetch(API + "/authenticate", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const result = await response.json();
+      console.log(result);
+
+      if (result.success) {
+        setLocation("TUNNEL"); // Move to final scene
+      } else {
+        throw new Error(result.message);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const value = {
+    location,
+    signup,
+    authenticate,
+  };
+
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
 export function useAuth() {
   const context = useContext(AuthContext);
-  if (!context) throw Error("useAuth must be used within an AuthProvider");
+  if (!context) throw new Error("useAuth must be used within an AuthProvider");
   return context;
 }
--- src/Entrance.jsx
+++ src/Entrance.jsx
@@ -1,6 +1,18 @@
-/** Users can enter their name to receive a token from the API. */
+/** Users can enter their name to receive a token from the API. Track users name
+ * 2)  call signup (name) when the form is submitted.
+ * useAuth gives access to signup
+ */
+import { useState } from "react";
+import { useAuth } from "./AuthContext";
+
 export default function Entrance() {
-  // TODO: call signup when form is submitted
+  const [name, setName] = useState("");
+  const { signup } = useAuth();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    await signup(name); // from index demo
+  }
 
   return (
     <>
@@ -15,12 +27,16 @@ export default function Entrance() {
         fixed on you. The one on the left opens its mouth, and with a deep,
         rumbling voice, it asks, "Who approaches? Speak your name."
       </p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Name
-          <input name="name" />
+          <input
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </label>
-        <button>Respond</button>
+        <button type="submit">Respond</button>
       </form>
     </>
   );
